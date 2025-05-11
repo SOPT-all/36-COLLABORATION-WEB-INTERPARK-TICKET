73d8b9823b45de8363312f625df25e372306e43f
jllee000 <jllee000@naver.com>
2025-05-12 00:05:34 +0900

    chore : set up eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,3 @@
-// import js from "@eslint/js";
-// import globals from "globals";
-// import tseslint from "typescript-eslint";
-// import pluginReact from "eslint-plugin-react";
-// import json from "@eslint/json";
-// import css from "@eslint/css";
-// import { defineConfig } from "eslint/config";
-
-// export default defineConfig([
-//   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-//   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-//   tseslint.configs.recommended,
-//   pluginReact.configs.flat.recommended,
-//   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-//   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-// ]);
-
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
