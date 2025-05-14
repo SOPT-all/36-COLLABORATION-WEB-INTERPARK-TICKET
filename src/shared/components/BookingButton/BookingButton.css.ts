6328c5c2d6fba9118fc9d14d42fb62bd094b7637
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-14 17:44:12 +0900

    feat: 예매하기 버튼 컴포넌트 구현

---
+++ src/shared/components/BookingButton/BookingButton.css.ts
@@ -0,0 +1,15 @@
+import { fontStyle } from '@/shared/styles/fontStyle';
+import { vars } from '@/shared/styles/tokens.css';
+import { style } from '@vanilla-extract/css';
+
+export const bookingButton = style({
+  padding: '0.6rem 1.9rem',
+  boxSizing: 'border-box',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  backgroundColor: vars.color.blue90,
+  borderRadius: '0.4rem',
+  color: vars.color.white,
+  ...fontStyle('b8_sb_14'),
+});
