40faeee85c5ac9958126e5902bf1b9dc7670a529
soyyyyy <da5868@naver.com>
2025-05-14 23:45:26 +0900

    feat: 예금주 정보 카드 컴포넌트 제작

--- src/pages/ComponentTest/ComponentTest.tsx
+++ src/pages/ComponentTest/ComponentTest.tsx
@@ -1,38 +1,18 @@
-import SeatSelectChip from '@/shared/components/SeatSelectChip/SeatSelectChip';
-import { useState } from 'react';
+import BankInfoCard from '@/shared/components/BankInfoCard/BankInfoCard';
 
 const ComponentTest = () => {
-  const [selectedSeat, setSelectedSeat] = useState<'S' | 'R' | null>(null);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-      }}
-    >
       <div
         style={{
           display: 'flex',
           padding: '1rem',
           gap: '0.6rem',
-          backgroundColor: '#3B3D44',
         }}
       >
-        <SeatSelectChip
-          seatType="S"
-          isSelected={selectedSeat === 'S'}
-          onClick={() => setSelectedSeat('S')}
-        />
+        <BankInfoCard />
 
-        <SeatSelectChip
-          seatType="R"
-          isSelected={selectedSeat === 'R'}
-          onClick={() => setSelectedSeat('R')}
-        />
       </div>
-    </div>
   );
 };
 
