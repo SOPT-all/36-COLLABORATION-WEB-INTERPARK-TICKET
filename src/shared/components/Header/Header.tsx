e8760cdd8433de3f141199c1c27e24e68ff8fda6
jllee000 <jllee000@naver.com>
2025-05-15 00:29:41 +0900

    feat : 헤더-공연 제목  컴포넌트

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,17 +1,22 @@
 import * as styles from './Header.css';
-
+import BackIcon from '@/shared/assets/icon/ic_back_white_24.svg';
+import CloseIcon from '@/shared/assets/icon/ic_close_white_24.svg';
 interface Props extends React.HTMLProps<HTMLDivElement> {
   title: React.ReactNode;
   onBackClick?: () => void;
   onCloseClick?: () => void;
 }
 
-const Header = ({ title, onBackClick, onCloseClick, ...props }: Props) => {
+const Header = ({ title, onBackClick, onCloseClick }: Props) => {
   return (
-    <div>
-      <div></div>
-      <div>연극프라이드</div>
-      <div></div>
+    <div className={styles.HeaderWrapper}>
+      <div onClick={onBackClick}>
+        <img src={BackIcon} alt="뒤로가기" />
+      </div>
+      <div className={styles.HeaderTxt}>{title}</div>
+      <div onClick={onCloseClick}>
+        <img src={CloseIcon} alt="닫기" />
+      </div>
     </div>
   );
 };
