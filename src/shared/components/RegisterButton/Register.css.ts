0f10d3fd77d41c9441d8c9fdcea3df9aac912f15
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-14 17:44:31 +0900

    feat: 등록 버튼 컴포넌트 구현

---
+++ src/shared/components/RegisterButton/Register.css.ts
@@ -0,0 +1,14 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/shared/styles/tokens.css';
+import { fontStyle } from '@/shared/styles/fontStyle';
+
+export const registerButton = style({
+  padding: '0.5rem 2rem',
+  boxSizing: 'border-box',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: vars.color.blue120,
+  borderRadius: '0.4rem',
+  color: vars.color.white,
+  ...fontStyle('b12_sb_12'),
+});
