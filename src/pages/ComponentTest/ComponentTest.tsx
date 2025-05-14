27c0ab1aed51f9a07c70b5e946595e39f0fac78d
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-14 14:43:16 +0900

    feat: 컴포넌트 테스트를 위한 임시 페이지 추가

--- src/pages/ComponentTest/ComponentTest.tsx
+++ src/pages/ComponentTest/ComponentTest.tsx
@@ -1,18 +1,38 @@
-import HomeAddButton from "@/shared/components/HomeAddButton/HomeAddButton";
+import SeatSelectChip from '@/shared/components/SeatSelectChip/SeatSelectChip';
+import { useState } from 'react';
 
 const ComponentTest = () => {
+  const [selectedSeat, setSelectedSeat] = useState<'S' | 'R' | null>(null);
 
   return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+      }}
+    >
       <div
         style={{
           display: 'flex',
           padding: '1rem',
           gap: '0.6rem',
+          backgroundColor: '#3B3D44',
         }}
       >
-        <HomeAddButton keyword="할인티켓" />
+        <SeatSelectChip
+          seatType="S"
+          isSelected={selectedSeat === 'S'}
+          onClick={() => setSelectedSeat('S')}
+        />
 
+        <SeatSelectChip
+          seatType="R"
+          isSelected={selectedSeat === 'R'}
+          onClick={() => setSelectedSeat('R')}
+        />
       </div>
+    </div>
   );
 };
 
