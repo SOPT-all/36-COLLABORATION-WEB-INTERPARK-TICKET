0e02ed1d857ed893097cf6d56ed924f44dcd7562
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-14 15:31:59 +0900

    fix: 메인 페이지 폰트명 수정

--- src/pages/Main/MainPage.css.ts
+++ src/pages/Main/MainPage.css.ts
@@ -1,8 +1,8 @@
-import { style } from "@vanilla-extract/css";
-import { fontStyle } from "@styles/fontStyle";
-import { vars } from "@styles/tokens.css";
+import { style } from '@vanilla-extract/css';
+import { fontStyle } from '@styles/fontStyle';
+import { vars } from '@styles/tokens.css';
 
 export const h1 = style({
-  ...fontStyle("h_sb_38"),
+  ...fontStyle('h1_sb_38'),
   color: vars.color.blue50,
 });
