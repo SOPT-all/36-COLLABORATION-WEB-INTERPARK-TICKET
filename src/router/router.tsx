c358f59d01bf703dc48aa4f1332420df800c7edf
karnelll <165611407+karnelll@users.noreply.github.com>
2025-05-15 05:52:42 +0900

    chore: 테스트용 Payment 파일 삭제 및 라우트 제거

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import MainPage from "@/pages/Main/MainPage";
-import PaymentTestPage from "@/pages/Payment/PaymentTestPage"; 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
-  },
-  {
-    path: "/payment-test", 
-    element: <PaymentTestPage />,
   },
 ]);
 
