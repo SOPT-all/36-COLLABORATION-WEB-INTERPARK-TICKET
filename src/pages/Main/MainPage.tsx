2feb10064f781723b14e7ea37cc850bb8ce03c4a
jllee000 <jllee000@naver.com>
2025-05-15 00:02:29 +0900

    add :헤더  아이콘 및 이미지 추가

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,8 +1,10 @@
-import { h1 } from "./MainPage.css";
+import { h1 } from './MainPage.css';
+import Header from '@/shared/components/Header/Header';
 
 function MainPage() {
   return (
     <div>
+      <Header title="연극프라이드" />
       <h1 className={h1}>인터파크 티켓</h1>
     </div>
   );
--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -1,18 +1,19 @@
-import type { ReactHTMLElement } from 'react'
-import * as styles from './'Header.css';
+import * as styles from './Header.css';
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
-  title : React.ReactNode;
-  onBackClick? : () => void;
-  onCloseClick? : () => void;
+  title: React.ReactNode;
+  onBackClick?: () => void;
+  onCloseClick?: () => void;
 }
 
-const Header= ({ title, onBackClick, onCloseClick, ...props }: Props) =>{
-    return (
-        <div>
-            <div></div>
-            <div>연극프라이드</div>
-            <div></div>
-        </div>
-    )
-}
+const Header = ({ title, onBackClick, onCloseClick, ...props }: Props) => {
+  return (
+    <div>
+      <div></div>
+      <div>연극프라이드</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
