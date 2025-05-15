43e68ab0b301dc63c402eae2b68dc7cc4cc90bd9
jllee000 <jllee000@naver.com>
2025-05-15 11:59:37 +0900

    feat : 메인페이지 수정사항 제거

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,9 +1,7 @@
 import { h1 } from './MainPage.css';
-import HomeHeader from '@/shared/components/Header/HomeHeader/HomeHeader';
 function MainPage() {
   return (
     <div>
-      <HomeHeader />
       <h1 className={h1}>인터파크 티켓</h1>
     </div>
   );
