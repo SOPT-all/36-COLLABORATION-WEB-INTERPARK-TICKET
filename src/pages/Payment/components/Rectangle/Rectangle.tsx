e8c898a9332e4581f7b3d7cc3074c54a248f4020
Kim Yeji <101444425+mnbvcxzyj@users.noreply.github.com>
2025-05-16 03:14:25 +0900

    Feat/rectangle component/#46 (#52)
    * feat: 드롭다운 컴포넌트 구현
    
    * fix: 경로 수정 및 대소문자 확인
    
    * chore: 테스트용 Payment 파일 삭제 및 라우트 제거
    
    * feat: Rectangle 94/95 컴포넌트 및 벡터 svg 파일 추가
    
    ---------
    
    Co-authored-by: karnelll <165611407+karnelll@users.noreply.github.com>

---
+++ src/pages/Payment/components/Rectangle/Rectangle.tsx
@@ -0,0 +1,9 @@
+import { rectangle94, rectangle95 } from './Rectangle.css';
+
+export const Rectangle95 = () => {
+  return <div className={rectangle95} />;
+};
+
+export const Rectangle94 = () => {
+  return <div className={rectangle94} />;
+};
