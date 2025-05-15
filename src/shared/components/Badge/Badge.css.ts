54b213a675ce822e700b9ee4652686a103d4424d
jllee000 <jllee000@naver.com>
2025-05-15 23:35:43 +0900

    fix : css 분기 최적화 수정

--- src/shared/components/Badge/Badge.css.ts
+++ src/shared/components/Badge/Badge.css.ts
@@ -1,19 +1,26 @@
-import { style } from '@vanilla-extract/css';
-
-export const BadgeWrapper = style({
-  padding: '0.4rem 0.5rem',
-  textAlign: 'center',
-  borderRadius: '0.4rem',
-  fontSize: '1rem',
-  fontWeight: 700,
-  lineHeight: '1.1rem',
-});
-export const red = style({
-  color: '#FF322E',
-  backgroundColor: '#FFEAEA',
-});
-
-export const blue = style({
-  color: '#4154FF',
-  backgroundColor: '#F0F1FF',
+import { recipe } from '@vanilla-extract/recipes';
+import { vars } from '@/shared/styles/tokens.css';
+import { fontStyle } from '@/shared/styles/fontStyle';
+export const badgeWrapper = recipe({
+  base: {
+    padding: '0.4rem 0.5rem',
+    textAlign: 'center',
+    borderRadius: '0.4rem',
+    ...fontStyle('c1_b_10'),
+  },
+  variants: {
+    type: {
+      Hot: {
+        color: vars.color.red30,
+        backgroundColor: vars.color.red10,
+      },
+      단독판매: {
+        color: vars.color.blue90,
+        backgroundColor: vars.color.blue10,
+      },
+    },
+  },
+  defaultVariants: {
+    type: 'Hot',
+  },
 });
--- src/shared/components/Badge/Badge.tsx
+++ src/shared/components/Badge/Badge.tsx
@@ -1,21 +1,11 @@
 import * as styles from './Badge.css';
 
-interface Props extends React.HTMLProps<HTMLDivElement> {
+interface Props {
   type: 'Hot' | '단독판매';
 }
 
 const Badge = ({ type }: Props) => {
-  let badgeType = '';
-  switch (type) {
-    case 'Hot':
-      badgeType = styles.red;
-      break;
-    case '단독판매':
-      badgeType = styles.blue;
-      break;
-  }
-
-  return <span className={`${styles.BadgeWrapper} ${badgeType}`}>{type}</span>;
+  return <span className={styles.badgeWrapper({ type })}>{type}</span>;
 };
 
 export default Badge;
