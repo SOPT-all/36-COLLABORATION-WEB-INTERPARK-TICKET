9b1554213535fc6cef0b6b68e2e8bd711611616d
karnelll <165611407+karnelll@users.noreply.github.com>
2025-05-15 21:17:53 +0900

    fix: 리뷰 반영

--- src/pages/Payment/components/Container/Container.css.ts
+++ src/pages/Payment/components/Container/Container.css.ts
@@ -3,8 +3,8 @@ import { style } from "@vanilla-extract/css";
 export const Container = style({
   display: "flex",
   flexWrap: "wrap",
-  width: "33.7rem",
-  alignItems: "flex-start",
-  alignContent: "flex-start",
-  gap: "0.6rem 0.3rem",
+  gap: "0.6rem 0.3rem",             
+  width: "33.7rem",                  
+  alignItems: "flex-start",        
+  alignContent: "flex-start",     
 });
