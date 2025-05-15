c301069962b5399c3a4542b10f2aa26c2115436e
SEOHEE CHOI <165611407+karnelll@users.noreply.github.com>
2025-05-16 02:57:10 +0900

    feat: dropdown component 구현 (#34)
    * feat: 드롭다운 컴포넌트 구현
    
    * [fix] 폰트 수정
    
    * [chore] 주석 제거
    
    * fix: 경로 수정 및 대소문자 확인
    
    * chore: 테스트용 Payment 파일 삭제 및 라우트 제거
    
    * [fix] 리스트체크 리뷰 반영
    
    * [fix] 리스트 체크 리뷰 반영
    
    * fix: 리스트체크 미들 리뷰 반영
    
    * fix: 리스트체크 미들 리뷰 반영
    
    * fix: 리스트인포 리뷰 반영
    
    * fix: 리스트인포 리뷰 반영
    
    * fix: 리스트포인트 리뷰 반영
    
    * fix: 리스트 포인트 리뷰 반영
    
    * fix: 리스트 포인트 리뷰 반영
    
    * [fix] 빌드 오류 수정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,5 @@
-import { createBrowserRouter } from 'react-router';
-import MainPage from '@/pages/Main/MainPage';
+import { createBrowserRouter } from "react-router";
+import MainPage from "@/pages/Main/MainPage";
 
 const router = createBrowserRouter([
   {
