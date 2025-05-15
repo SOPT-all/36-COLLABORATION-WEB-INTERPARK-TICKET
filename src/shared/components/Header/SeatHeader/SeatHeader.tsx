345e11006dfe53a24614b35ebdbdb23bde4fa4b4
jllee000 <jllee000@naver.com>
2025-05-15 10:05:19 +0900

    fix : 빌드에러 코드수정

--- src/shared/components/Header/SeatHeader/SeatHeader.tsx
+++ src/shared/components/Header/SeatHeader/SeatHeader.tsx
@@ -1,7 +1,7 @@
 import * as styles from './SeatHeader.css';
 import BackIcon from '@/shared/assets/icon/ic_back_white_24.svg';
 import CloseIcon from '@/shared/assets/icon/ic_close_white_24.svg';
-interface Props extends React.HTMLProps<HTMLDivElement> {
+interface Props {
   title: React.ReactNode;
   onBackClick?: () => void;
   onCloseClick?: () => void;
