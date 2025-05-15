b781109fdbce5380df62b3e56f350591ceb11d11
Soy <90364636+soyyyyy@users.noreply.github.com>
2025-05-16 02:51:09 +0900

    feat: 좌석 선택 bottom sheet 컴포넌트 구현  (#32)
    * feat: 컴포넌트 테스트를 위한 임시 페이지 추가
    
    * feat: 좌석 선택 공용 컴포넌트 구현
    
    * feat: 예금주 정보 카드 컴포넌트 제작
    
    * feat: 홈 더보기 버튼 컴포넌트
    
    * feat: 좌석 선택 바텀 시트 컴포넌트
    
    * test: 테스트 컴포넌트 삭제
    
    * refactor: 리뷰 반영
    
    * refactor: 오타수정
    
    ---------
    
    Co-authored-by: mnbvcxzyj <mnbvcxzyj@gmail.com>

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,8 +1,16 @@
-import { h1 } from './MainPage.css';
+import SeatBottomSheet from "@/shared/components/SeatBottomSheet/SeatBottomSheet";
+import { h1 } from "./MainPage.css";
+
 function MainPage() {
   return (
     <div>
       <h1 className={h1}>인터파크 티켓</h1>
+      <SeatBottomSheet
+        placeInfo="예스24아트원 2관"
+        dateTime="2025.04.29 / 19:30"
+        seatInfo="R석 1층 F열 17"
+        price={66000}
+      />
     </div>
   );
 }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from 'react-router';
 import MainPage from '@/pages/Main/MainPage';
-import ComponentTest from '@/pages/ComponentTest/ComponentTest';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
-  },
-  {
-    path: '/component-test',
-    element: <ComponentTest />,
   },
 ]);
 
