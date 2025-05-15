0ae73cf6732b116d7177b374a3d3a6a9e9a6f582
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-16 04:25:58 +0900

    chore: 폰트 파일 및 전역 설정 추가

--- src/shared/styles/global.css.ts
+++ src/shared/styles/global.css.ts
@@ -1,2 +1,29 @@
-import './reset.css';
-import './layer.css';
+import { globalStyle } from '@vanilla-extract/css';
+import * as layers from './layer.css';
+import { vars } from './tokens.css';
+
+globalStyle('body', {
+  '@layer': {
+    [layers.reset]: {
+      margin: 0,
+      backgroundColor: vars.color.gray10,
+      fontFamily: vars.font.family.pretendard,
+      userSelect: 'none',
+      WebkitUserSelect: 'none',
+      MozUserSelect: 'none',
+      msUserSelect: 'none',
+    },
+  },
+});
+
+globalStyle('#root', {
+  '@layer': {
+    [layers.components]: {
+      maxWidth: '375px',
+      margin: '0 auto',
+      width: '100%',
+      minHeight: '100vh',
+      backgroundColor: vars.color.white,
+    },
+  },
+});
