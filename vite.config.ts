c566d07af72c7d538aeb746fc6552103f05fb689
SEOHEE CHOI <165611407+karnelll@users.noreply.github.com>
2025-05-16 01:29:28 +0900

    Revert "feat: rectangle component"

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), svgr(),],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       "@": "/src",
