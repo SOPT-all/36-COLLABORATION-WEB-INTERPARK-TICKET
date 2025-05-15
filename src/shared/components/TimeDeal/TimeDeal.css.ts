f8035f00c6a356fc96faed734dfa809882b6cdd6
jelly._.me <101315059+jllee000@users.noreply.github.com>
2025-05-16 02:44:48 +0900

    feat: 타임딜 뱃지 컴포넌트 (#49)
    * feat: 타임딜 뱃지 컴포넌트
    
    * fix : 코드 정렬

---
+++ src/shared/components/TimeDeal/TimeDeal.css.ts
@@ -0,0 +1,26 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/shared/styles/tokens.css';
+import { fontStyle } from '@/shared/styles/fontStyle';
+export const TimeDealWrapper = style({
+  flexDirection: 'row',
+  gap: 0,
+  alignItems: 'center',
+  display: 'inline-flex',
+});
+export const TimeDealTxt = style({
+  padding: '0.3rem 0.5rem',
+  ...fontStyle('b12_sb_12'),
+  color: vars.color.white,
+  backgroundColor: vars.color.red30,
+  borderRadius: '0.6rem 0 0 0.6rem',
+  border: ` 1px solid  ${vars.color.red20}`,
+});
+
+export const TimeDealCount = style({
+  padding: '0.3rem 0.5rem',
+  ...fontStyle('b12_sb_12'),
+  color: vars.color.red30,
+  borderRadius: ' 0 0.6rem 0.6rem 0',
+  border: `1px solid  ${vars.color.red20}`,
+  borderLeft: 'none',
+});
