2342151700947a9b06b5aa62fb926814324747a6
Soy <90364636+soyyyyy@users.noreply.github.com>
2025-05-16 15:24:17 +0900

    feat: 홈 전체보기 버튼 컴포넌트 추가 (#59)

---
+++ src/shared/components/HomeMoreButton/HomeMoreButton.tsx
@@ -0,0 +1,19 @@
+import * as styles from './HomeMoreButton.css';
+import ArrowRight from "@assets/icon/ic_arrow_right_gray70_16.svg";
+
+type KeywordType = '뮤지컬 랭킹' | '할인티켓' | '오픈 예정 공연';
+interface ViewMoreProps {
+    keyword: KeywordType;
+}
+
+const HomeAddButton = ({ keyword }: ViewMoreProps) => {
+
+    return (
+        <button className={styles.button}>
+            <p className={styles.text}>{keyword} 전체보기</p>
+            <img src={ArrowRight} alt='ARIcon' className={styles.ARIcon} />
+        </button>
+    )
+};
+
+export default HomeAddButton;
