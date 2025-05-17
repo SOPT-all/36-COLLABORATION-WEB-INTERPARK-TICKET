8312100cbcd94794b3fc170837e21b6f3616313a
karnelll <165611407+karnelll@users.noreply.github.com>
2025-05-17 13:19:09 +0900

    chore: 빌드오류해결

--- src/pages/Payment/components/CustomCheckbox/CustomCheckbox.tsx
+++ src/pages/Payment/components/CustomCheckbox/CustomCheckbox.tsx
@@ -3,9 +3,9 @@ import CheckedIcon from '@/shared/assets/icon/checkbox_fill.svg?react';
 import UncheckedIcon from '@/shared/assets/icon/checkbox_unfill.svg?react';
 
 interface CustomCheckboxProps {
-  checked?: boolean; 
-  defaultChecked?: boolean; 
-  onChange?: (checked: boolean) => void; 
+  checked?: boolean;
+  defaultChecked?: boolean;
+  onChange?: (checked: boolean) => void;
 }
 
 export default function CustomCheckbox({
