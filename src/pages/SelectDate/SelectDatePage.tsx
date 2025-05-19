21d434767a23f465243496dbd18cf7e6d6f7d829
jllee000 <jllee000@naver.com>
2025-05-20 00:14:29 +0900

    feat : 예약 정보 컴포넌트 연결 및 props전달

--- src/pages/SelectDate/SelectDatePage.tsx
+++ src/pages/SelectDate/SelectDatePage.tsx
@@ -3,6 +3,7 @@ import SeatHeader from '@/shared/components/Header/SeatHeader/SeatHeader';
 import CardContentInfo from '@/shared/components/CardContentInfo/CardContentInfo';
 import Calendar from '@/shared/components/Calendar/Calendar';
 import RightIcon from '@/shared/assets/icon/ic_arrow_right_gray70_16.svg';
+import DateReservationCard from '@/shared/components/DateReservationCard/DateReservationCard';
 function SelectDatePage() {
   const performanceData = {
     performanceTitle: '연극제목',
@@ -13,6 +14,15 @@ function SelectDatePage() {
     runningTime: '시간',
     ageLimit: 18,
   };
+  const performanceTime = '2025년 4월 1일';
+  const authors = '작가 이름';
+
+  // 좌석 타입 정보
+  const seatTypes = [
+    { grade: 'VIP', seatCount: 50, price: 100000 },
+    { grade: '일반', seatCount: 100, price: 50000 },
+    { grade: '학생', seatCount: 150, price: 20000 },
+  ];
 
   return (
     <div>
@@ -28,6 +38,11 @@ function SelectDatePage() {
         </section>
         <div className={styles.infoArea}>
           <div className={styles.infoDefault}>공연 날짜를 선택해주세요</div>
+          <DateReservationCard
+            performanceTime={performanceTime}
+            authors={authors}
+            seatTypes={seatTypes}
+          />
         </div>
       </main>
     </div>
