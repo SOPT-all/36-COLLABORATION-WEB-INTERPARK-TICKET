d6a227a13f48204655b335257bf333e5a579b8da
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-20 02:22:52 +0900

    chore: 쿼리키 상수화

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './shared/styles/global.css';
 import './shared/styles/font.css';
 import './shared/styles/reset.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './shared/network/queryClient';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
