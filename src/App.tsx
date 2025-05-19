a34bfc1f07026fd96bf35f7cfae727bb4a8731ac
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-20 02:23:08 +0900

    chore: 토스트 컨테이너 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { RouterProvider } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 import router from './router/router';
-
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        closeOnClick={true}
+        pauseOnHover={false}
+        theme="colored"
+      />
+    </>
+  );
 }
 
 export default App;
