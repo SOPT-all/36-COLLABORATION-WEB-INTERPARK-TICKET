eba603bb13f82f8f501b962f68d6dd09ded31865
karnelll <165611407+karnelll@users.noreply.github.com>
2025-05-20 07:12:47 +0900

    fix: 폰트수정

--- src/pages/Payment/components/InfoRowTitle/InfoRowTitle.css.ts
+++ src/pages/Payment/components/InfoRowTitle/InfoRowTitle.css.ts
@@ -1,5 +1,6 @@
 import { style } from '@vanilla-extract/css';
 import { vars } from '@/shared/styles/tokens.css';
+import { fontStyle } from '@/shared/styles/fontStyle';
 
 export const infoRowTitleWrapper = style({
   display: 'flex',
@@ -9,23 +10,13 @@ export const infoRowTitleWrapper = style({
 });
 
 export const infoRowTitleText = style({
-  fontFamily: vars.font.family.pretendard,
-  fontSize: '1.6rem',
-  fontStyle: 'normal',
-  fontWeight: 700,
-  lineHeight: '2.24rem',
-  letterSpacing: '-0.032rem',
+  ...fontStyle('b2_b_16'),
   color: vars.color.gray80,
 });
 
 export const infoRowSubText = style({
+  ...fontStyle('b12_sb_12'),
   color: vars.color.gray60,
-  fontFamily: vars.font.family.pretendard,
-  fontSize: '1.2rem',
-  fontStyle: 'normal',
-  fontWeight: 600,
-  lineHeight: '1.92rem',
-  letterSpacing: '-0.024rem',
   marginLeft: 'auto',
   textAlign: 'right',
   paddingRight: '1.5rem',
