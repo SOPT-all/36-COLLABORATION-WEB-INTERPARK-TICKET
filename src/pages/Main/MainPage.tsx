9dd57f7287e6a9572860977ee2b62d788baaf8f2
jllee000 <jllee000@naver.com>
2025-05-19 21:54:34 +0900

    feat : 메인페이지에서 코드 수정  제거

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,9 +1,7 @@
 import { h1 } from './MainPage.css';
-import Calendar from '@/shared/components/Calendar/Calendar';
 function MainPage() {
   return (
     <div>
-      <Calendar />
       <h1 className={h1}>인터파크 티켓</h1>
     </div>
   );
