022d810fafd6eae10d86fdc15e05ae5e1ab26a59
jllee000 <jllee000@naver.com>
2025-05-20 00:27:22 +0900

    feat : 달력 클릭 여부에 따른 예매박스 핸들

--- src/pages/SelectDate/SelectDatePage.tsx
+++ src/pages/SelectDate/SelectDatePage.tsx
@@ -5,7 +5,9 @@ import Calendar from '@/shared/components/Calendar/Calendar';
 import RightIcon from '@/shared/assets/icon/ic_arrow_right_gray70_16.svg';
 import TimerIcon from '@/shared/assets/icon/ic_ wait_blue70_36.svg';
 import DateReservationCard from '@/shared/components/DateReservationCard/DateReservationCard';
+import { useState } from 'react';
 function SelectDatePage() {
+  const [isSelected, setIsSelected] = useState(false);
   const performanceData = {
     performanceTitle: '연극제목',
     performanceLocation: '공연장 위치',
@@ -24,6 +26,9 @@ function SelectDatePage() {
     { grade: '일반', seatCount: 100, price: 50000 },
     { grade: '학생', seatCount: 150, price: 20000 },
   ];
+  const handleSelectDate = (selected: boolean) => {
+    setIsSelected(selected); // true로 바뀌는거면! 그러면 이제 예매박스 띄우기
+  };
 
   return (
     <div>
@@ -35,25 +40,31 @@ function SelectDatePage() {
             <span className={styles.monthTxt}>2025.04</span>
             <img className={styles.monthIcon} src={RightIcon} alt={'다음 달'} />
           </div>
-          <Calendar />
+          <Calendar onDateSelect={handleSelectDate} />
         </section>
         <div className={styles.infoArea}>
-          <div className={styles.infoDefault}>공연 날짜를 선택해주세요</div>
-          <DateReservationCard
-            performanceTime={performanceTime}
-            authors={authors}
-            seatTypes={seatTypes}
-          />
-          <div className={styles.waitNoti}>
-            <img
-              className={styles.waitIcon}
-              src={TimerIcon}
-              alt="타이머아이콘"
-            />
-            <span className={styles.waitTxt}>
-              예매대기 신청은 관람일 3일 전까지만 가능해요!
-            </span>
-          </div>
+          {!isSelected && (
+            <div className={styles.infoDefault}>공연 날짜를 선택해주세요</div>
+          )}
+          {isSelected && (
+            <div>
+              <DateReservationCard
+                performanceTime={performanceTime}
+                authors={authors}
+                seatTypes={seatTypes}
+              />
+              <div className={styles.waitNoti}>
+                <img
+                  className={styles.waitIcon}
+                  src={TimerIcon}
+                  alt="타이머아이콘"
+                />
+                <span className={styles.waitTxt}>
+                  예매대기 신청은 관람일 3일 전까지만 가능해요!
+                </span>
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </div>
