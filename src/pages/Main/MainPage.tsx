bbdfda9eddfb449dc97f5791ad2a16f0be6b7519
soyyyyy <da5868@naver.com>
2025-05-19 22:12:00 +0900

    fix: 빌드 에러 수정 - import 순서 다시

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,7 +1,7 @@
+import * as styles from './MainPage.css'
 import HomeHeader from '@/shared/components/Header/HomeHeader/HomeHeader';
 import MainBanner from '@/shared/assets/icon/home_card_banner.svg';
 import { CATEGORY_ITEMS } from '@/shared/constants/Categories';
-import * as styles from './MainPage.css'
 
 function MainPage() {
   return (
