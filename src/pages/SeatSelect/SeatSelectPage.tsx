826555b580bdbe13b04e85c93832757ace428299
Kim Yeji <101444425+mnbvcxzyj@users.noreply.github.com>
2025-05-21 00:05:21 +0900

    feat: 좌석 선택 페이지 구현  (#89)
    * feat: 상단 좌석 선택 칩
    
    * feat: 팝업 아이콘 버튼 추가
    
    * feat: 좌석 필터링 기능 구현
    
    * feat: 시계 아이콘 클릭 시 팝업 기능 구현
    
    * fix: globalStyle 화면 너비 조정 및 스크롤 방지 추가
    
    * feat: 좌석 선택시 하단 sheet 기능 구현
    
    * feat: 하단 바텀 시트 애니메이션 방식 변경
    
    * chore: 좌석 선택 헤더 수정 및 뒤로가기 연결

--- src/pages/SeatSelect/SeatSelectPage.tsx
+++ src/pages/SeatSelect/SeatSelectPage.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import ic_wait_blue70_36 from '@assets/icon/ic_ wait_blue70_36.svg';
+import { useNavigate } from 'react-router';
 import SeatCard from './components/SeatCard/SeatCard';
+import SeatSelectChip from './components/SeatSelectChip/SeatSelectChip';
 import StageText from './components/StageText/StageText';
 import * as styles from './SeatSelectPage.css';
-import type { SeatData } from './types/SeatData';
+import Popup from './components/Popup/Popup';
+import SeatBottomSheet from './components/SeatBottomSheet/SeatBottomSheet';
+import type { SeatData, SeatGradeFilter, SeatPosition } from './types/SeatData';
 import SeatHeader from '@/shared/components/Header/SeatHeader/SeatHeader';
 
 const seatData: SeatData[] = [
@@ -46,8 +52,8 @@ const seatData: SeatData[] = [
     ],
   },
   {
-    grade: 'S',
-    price: 30000,
+    grade: 'R',
+    price: 48000,
     row: 'C',
     availability: [
       false,
@@ -168,12 +174,91 @@ const seatData: SeatData[] = [
 ];
 
 const SeatSelectPage = () => {
+  const navigate = useNavigate();
+
+  const [selectedSeatType, setSelectedSeatType] =
+    useState<SeatGradeFilter>(null);
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const [selectedSeatInfo, setSelectedSeatInfo] = useState<
+    string | undefined
+  >();
+  const [selectedSeatPrice, setSelectedSeatPrice] = useState<
+    number | undefined
+  >();
+
+  const [selectedSeat, setSelectedSeat] = useState<SeatPosition | null>(null);
+
+  const handleSelectSeat = (
+    row: string,
+    index: number,
+    grade: SeatGradeFilter,
+    price: number
+  ) => {
+    setSelectedSeat({ row, index });
+    setSelectedSeatInfo(`${grade}석 ${row}열 ${index + 1}`);
+    setSelectedSeatPrice(price);
+  };
+
+  const handleRetryClick = () => {
+    setSelectedSeat(null);
+    setSelectedSeatInfo(undefined);
+    setSelectedSeatPrice(undefined);
+    setSelectedSeatType(null);
+  };
+
+  const handleSeatTypeClick = (type: SeatGradeFilter) => {
+    setSelectedSeatType((prev) => (prev === type ? null : type));
+  };
+
+  const handleWaitIconClick = () => {
+    setShowPopup((prev) => !prev);
+  };
+
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
   return (
     <div className={styles.pageWrapper}>
-      <SeatHeader title="연극 이름" />
+      <SeatHeader title="연극 이름" onBackClick={handleBackClick} />
       <main className={styles.mainContent}>
+        <div className={styles.seatSelectWrapper}>
+          <SeatSelectChip
+            seatType="R"
+            isSelected={selectedSeatType === 'R'}
+            onClick={() => handleSeatTypeClick('R')}
+          />
+          <SeatSelectChip
+            seatType="S"
+            isSelected={selectedSeatType === 'S'}
+            onClick={() => handleSeatTypeClick('S')}
+          />
+        </div>
+
         <StageText />
-        <SeatCard seats={seatData} />
+
+        <SeatCard
+          seats={seatData}
+          selectedGrade={selectedSeatType}
+          selected={selectedSeat}
+          onSelectSeat={handleSelectSeat}
+        />
+
+        <button className={styles.waitIcon} onClick={handleWaitIconClick}>
+          <img src={ic_wait_blue70_36} alt="wait" />
+        </button>
+
+        <SeatBottomSheet
+          placeInfo="예스24아트원 2관"
+          dateTime="2025.07.10 (수) 19:30"
+          seatInfo={selectedSeatInfo}
+          price={selectedSeatPrice}
+          onRetryClick={handleRetryClick}
+        />
+
+        {showPopup && <Popup onClose={handleWaitIconClick} />}
       </main>
     </div>
   );
--- src/pages/SeatSelect/components/SeatBottomSheet/SeatBottomSheet.tsx
+++ src/pages/SeatSelect/components/SeatBottomSheet/SeatBottomSheet.tsx
@@ -1,12 +1,14 @@
-import ChangeButton from '../ChangeButton/ChangeButton';
-import SmallButton from '../SmallButton/SmallButton';
+import clsx from 'clsx';
 import * as styles from './SeatBottomSheet.css';
+import ChangeButton from '@/shared/components/ChangeButton/ChangeButton';
+import SmallButton from '@/shared/components/SmallButton/SmallButton';
 
 interface SeatBottomSheetProps {
   placeInfo: string;
   dateTime: string;
   seatInfo?: string;
   price?: number;
+  onRetryClick: () => void;
 }
 
 const SeatBottomSheet = ({
@@ -14,17 +16,23 @@ const SeatBottomSheet = ({
   dateTime,
   seatInfo,
   price,
+  onRetryClick,
 }: SeatBottomSheetProps) => {
   const isSeatSelected = !!seatInfo;
 
   return (
-    <div className={styles.sheetWrapper}>
+    <div
+      className={clsx(
+        styles.sheetWrapper,
+        isSeatSelected
+          ? styles.sheetWrapperExpanded
+          : styles.sheetWrapperCollapsed
+      )}
+    >
       <div className={styles.locationWrapper}>
         <p className={styles.locationText}>{placeInfo}</p>
       </div>
-
       <hr className={styles.line} />
-
       <div className={styles.infoWrapper}>
         <div className={styles.infoContainer}>
           <p className={styles.infoText({ state: 'default' })}>좌석</p>
@@ -45,28 +53,28 @@ const SeatBottomSheet = ({
         </div>
       </div>
 
-      {isSeatSelected && (
-        <div className={styles.bottomArea}>
-          <div className={styles.priceContainer}>
-            <p className={styles.infoText({ state: 'default' })}>결제금액</p>
-            {price !== undefined ? (
-              <p className={styles.priceText.normal}>
-                총{' '}
-                <span className={styles.priceText.strong}>
-                  {price.toLocaleString()}
-                </span>
-                원
-              </p>
-            ) : (
-              <p className={styles.priceText.normal}>가격 정보 없음</p>
-            )}
-          </div>
-          <div className={styles.buttonGroup}>
-            <SmallButton variant="retry">재선택</SmallButton>
-            <SmallButton variant="next">다음</SmallButton>
-          </div>
+      <div className={styles.bottomArea({ open: isSeatSelected })}>
+        <div className={styles.priceContainer}>
+          <p className={styles.infoText({ state: 'default' })}>결제금액</p>
+          {price !== undefined ? (
+            <p className={styles.priceText.normal}>
+              총
+              <span className={styles.priceText.strong}>
+                {price.toLocaleString()}
+              </span>
+              원
+            </p>
+          ) : (
+            <p className={styles.priceText.normal}>가격 정보 없음</p>
+          )}
         </div>
-      )}
+        <div className={styles.buttonGroup}>
+          <SmallButton variant="retry" onClick={onRetryClick}>
+            재선택
+          </SmallButton>
+          <SmallButton variant="next">다음</SmallButton>
+        </div>
+      </div>
     </div>
   );
 };
