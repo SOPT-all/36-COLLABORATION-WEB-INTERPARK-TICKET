6888aaa548a45262e183800da7441ce115395e4b
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-21 02:52:16 +0900

    fix: API 세팅 수정

--- src/shared/network/request.ts
+++ src/shared/network/request.ts
@@ -27,8 +27,8 @@ export const request = async <T>(config: RequestConfig): Promise<T> => {
       data: body,
     });
 
-    console.log(`[성공] ${url} : ${response.data.message}`);
-    // toast.success(response.data.message);
+    console.log(`[성공] ${url} :`, response.data.data);
+    toast.success(response.data.message);
 
     return response.data.data;
   } catch (error: any) {
