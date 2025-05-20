1207be540b847f4eec4220c787d9a858d5244db6
soyyyyy <da5868@naver.com>
2025-05-20 17:11:06 +0900

    feat:홈- 오픈 예정 공연 섹션 구현

--- src/pages/Main/mockData/mockData.tsx
+++ src/pages/Main/mockData/mockData.tsx
@@ -1,5 +1,6 @@
 import { type NolPlayData } from '../components/NOL PLAY/NolPlaySection';
 import type { KeywordData } from '../components/KeyWord/KeyWordSection';
+import type { PlannedData } from '../components/Planned/PlannedSection';
 
 export const NOLdummyData: NolPlayData = {
   category: 'NOL PLAY',
@@ -61,6 +62,68 @@ export const keywordDummyData: KeywordData = {
       location: 'YES24 라이브홀',
       startDate: '2025-07-01',
       endDate: '07-01',
+    },
+  ],
+};
+
+export const plannedDummyData: PlannedData = {
+  category: '곧 오픈하는 공연',
+  getHomeResponseList: [
+    {
+      id: 9,
+      title: '연극<카포네 트릴로지>',
+      imageUrl:
+        'https://github.com/user-attachments/assets/1941e016-153e-44ef-9383-b44996094ef8',
+      startDate: '2025.06.01',
+      description: '공연 연장 및 마지막 티켓 오픈',
+      tagList: ['HOT', '단독판매'],
+    },
+    {
+      id: 10,
+      title:
+        '[Play & stay] 2025 Weverse Con dkdkdkdkdkdkdkdkkdsasadasdadadasdsk',
+      imageUrl:
+        'https://github.com/user-attachments/assets/1941e016-153e-44ef-9383-b44996094ef8',
+      startDate: '2025.08.01',
+      description: '일반 예매',
+      tagList: ['HOT', '단독판매'],
+    },
+    {
+      id: 11,
+      title:
+        '[Play & stay] 2025 Weverse Con dkdkdkdkdkdkdkdkkdsasadasdadadasdsk',
+      imageUrl:
+        'https://github.com/user-attachments/assets/1941e016-153e-44ef-9383-b44996094ef8',
+      startDate: '2025.08.01',
+      description: '일반 예매',
+      tagList: ['HOT', '단독판매'],
+    },
+    {
+      id: 12,
+      title: '[Play & stay] 2025...',
+      imageUrl:
+        'https://github.com/user-attachments/assets/1941e016-153e-44ef-9383-b44996094ef8',
+      startDate: '2025.08.01',
+      description: '일반 예매',
+      tagList: ['HOT', '단독판매'],
+    },
+    {
+      id: 13,
+      title: '[Play & stay] 2025...',
+      imageUrl:
+        'https://github.com/user-attachments/assets/1941e016-153e-44ef-9383-b44996094ef8',
+      startDate: '2025.08.01',
+      description: '일반 예매',
+      tagList: ['HOT', '단독판매'],
+    },
+    {
+      id: 14,
+      title: '[Play & stay] 2025...',
+      imageUrl:
+        'https://github.com/user-attachments/assets/1941e016-153e-44ef-9383-b44996094ef8',
+      startDate: '2025.08.01',
+      description: '일반 예매',
+      tagList: ['HOT', '단독판매'],
     },
   ],
 };
