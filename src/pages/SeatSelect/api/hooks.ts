e87c2c6b8fac74bbcbfcbb33160b3abe25eea629
Kim Yeji <101444425+mnbvcxzyj@users.noreply.github.com>
2025-05-21 00:00:06 +0900

    setting: 네트워크 세팅 및 예시 API 코드  (#87)
    * add: axios, tanstack query 설치
    
    * add : react-toastify 설치
    
    * feat: 기본 응답 세팅, request 구조 추상화
    
    * chore: 쿼리키 상수화
    
    * chore: 토스트 컨테이너 추가
    
    * chore: patch 메소드 추가
    
    * feat: 좌석 관련 API 추가
    
    * chore: 미사용 파일 삭제
    
    * chore: response 응답 타입 추가
    
    * chore: router import 절대경로 변경
    
    * chore: default export로 변경

---
+++ src/pages/SeatSelect/api/hooks.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { getSeatData, patchSelectedSeat } from './api';
+import { QUERY_KEY } from '@/shared/constants/queryKey';
+
+// 좌석 조회
+export const useSeatData = () => {
+  return useQuery({
+    queryKey: [QUERY_KEY.SEAT_STATUS],
+    queryFn: getSeatData,
+  });
+};
+
+// 좌석 선택
+export const usePatchSeatData = () => {
+  return useMutation({
+    mutationFn: patchSelectedSeat,
+    onSuccess: () => {
+      console.log('좌석 선택 성공');
+    },
+    onError: () => {
+      console.log('좌석 선택 실패');
+    },
+  });
+};
