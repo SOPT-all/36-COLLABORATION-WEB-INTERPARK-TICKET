2cd3e9828b4dee644ef4fd133e147ddb5deb3151
SEOHEE CHOI <165611407+karnelll@users.noreply.github.com>
2025-05-20 17:25:03 +0900

    feat: payment step2/#72 (#84)
    * feat: step2 구현
    
    * fix: 뷰 수정
    
    * fix: 빌드오류 및 코드 수정
    
    * fix: 폰트수정
    
    * fix: 빌드오류 수정
    
    * fix: 빌드오류수정
    
    * fix: 빌드 오류 해결 및 스타일 수정
    
    * fix: 불필요한 간격 제거
    
    * fix: 폴더명 수정
    
    * fix: 폴더명 수정
    
    * fix: 경롯수정
    
    * fix: 리뷰 반영
    
    * chore: 빌드오류수정
    
    * chore: 빌드오류수정

--- src/pages/Payment/Payment.css.tsx
+++ src/pages/Payment/Payment.css.tsx
@@ -1,24 +1,45 @@
 import { style } from '@vanilla-extract/css';
 import { LAYOUT } from '@/shared/styles/layoutConstants';
+import { vars } from '@/shared/styles/tokens.css';
 
 export const page = style({
   padding: `${LAYOUT.PADDING.LG} ${LAYOUT.PADDING.MD} ${LAYOUT.PADDING.LG}`,
   display: 'flex',
   flexDirection: 'column',
-  gap: LAYOUT.GAP.XL,
+  gap: LAYOUT.GAP.SS,
+  fontFamily: vars.font.family.pretendard,
 });
 
 export const mainContent = style({
-  marginTop: '5.6rem',
+  marginTop: LAYOUT.HEIGHT.SM,
   display: 'flex',
   flexDirection: 'column',
   width: '100%',
+  minHeight: `calc(100vh - ${LAYOUT.HEIGHT.SM})`,
+  position: 'relative',
+  backgroundColor: vars.color.white,
 });
 
 export const buttonContainer = style({
-  marginTop: '6rem',
-  paddingBottom: LAYOUT.PADDING.XL,
+  marginTop: LAYOUT.MARGIN.XL,
   display: 'flex',
   justifyContent: 'center',
   width: '100%',
+  position: 'sticky',
+  bottom: 0,
+  background: vars.color.white,
+  padding: `${LAYOUT.PADDING.MD} 0`,
+});
+
+export const sectionContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100%',
+  backgroundColor: vars.color.white,
+});
+
+export const checkboxContainer = style({
+  paddingLeft: LAYOUT.PADDING.MD_PLUS,
+  paddingTop: LAYOUT.PADDING.XL,
 });
