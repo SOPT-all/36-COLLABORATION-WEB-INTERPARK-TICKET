d33352727ef5388393909e73a7c842e9f4db9194
karnelll <165611407+karnelll@users.noreply.github.com>
2025-05-23 17:22:52 +0900

    chore: 빌드오류 수정

--- src/pages/Payment/components/TextField/TextField.tsx
+++ src/pages/Payment/components/TextField/TextField.tsx
@@ -40,7 +40,7 @@ export default function TextField({
     >
       <input
         className={clsx(styles.input, value && styles.inputHasText)}
-        type={numbersOnly ? "tel" : "text"}
+        type={numbersOnly ? 'tel' : 'text'}
         placeholder={placeholder}
         value={value}
         onChange={handleChange}
