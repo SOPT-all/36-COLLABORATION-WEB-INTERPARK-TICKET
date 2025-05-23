23ecb5ec3f2b72506505853e2e1b9e70a4648b14
jllee000 <jllee000@naver.com>
2025-05-23 20:01:55 +0900

    feat : 타임딜 카운트다운 로직

--- src/pages/Main/components/TimeDeal/TimeDeal.tsx
+++ src/pages/Main/components/TimeDeal/TimeDeal.tsx
@@ -2,13 +2,20 @@ import * as styles from './TimeDeal.css';
 
 interface Props {
   text: string;
-  time: string;
+  time: number;
 }
+const formatTime = (seconds: number) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+};
 const TimeDeal = ({ text, time }: Props) => {
   return (
     <span className={styles.TimeDealWrapper}>
       <span className={styles.TimeDealTxt}>{text}</span>
-      <span className={styles.TimeDealCount}>{time}</span>
+      <span className={styles.TimeDealCount}>{formatTime(time)}</span>
     </span>
   );
 };
